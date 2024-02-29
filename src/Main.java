07affd4001684d378585bcca0fd70e13c4e5a543
patryk <kpatryk042@gmail.com>
2024-02-29 16:08:04 +0100

    kot1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
+class Point{
+    int x;
+    int y;
+
+    Point(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+}
+
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        Point punkt = new Point(3,2);
+        System.out.println(punkt.x);
+        System.out.println(punkt.y);
     }
+
 }
