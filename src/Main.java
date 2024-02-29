aa0df909a715330377446c88c19ef4ebf44b2107
patryk <kpatryk042@gmail.com>
2024-02-29 16:34:54 +0100

    kot

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,3 @@
-class Point{
-    int x;
-    int y;
-
-    Point(int x, int y){
-        this.x = x;
-        this.y = y;
-    }
-}
 
 public class Main {
     public static void main(String[] args) {
