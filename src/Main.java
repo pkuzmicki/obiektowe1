4c54740fdaa937dff2ec7acffc893dd340b394f9
patryk <kpatryk042@gmail.com>
2024-03-07 15:58:17 +0100

    kot

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,11 @@ public class Main {
     public static void main(String[] args) {
         Point p1 = new Point(10, 10);
         Point p2 = new Point(100, 100);
+        Point p3 = p1;
+        p3.x = 5;
         Segment segment = new Segment(p1, p2);
-        System.out.println(segment.toSvg());
+        //System.out.println(segment.toSvg());
+        Point[] points = new Point[10];
 
     }
 
