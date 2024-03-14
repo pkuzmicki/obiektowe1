5403302f76f10951ebea151b77b8e4770b235280
patryk <kpatryk042@gmail.com>
2024-03-14 15:30:29 +0100

    kot

--- src/Main.java
+++ src/Main.java
@@ -2,16 +2,16 @@
 public class Main {
     public static void main(String[] args) {
        Style style = new Style("purple", "pink", 10.0);
-       Point point1 = new Point(1, 100);
-       Point point2 = new Point(1, 1);
-       Point point3 = new Point(100, 100);
-       Point point4 = new Point(100, 200);
+       Vec2 vec21 = new Vec2(1, 100);
+       Vec2 vec22 = new Vec2(1, 1);
+       Vec2 vec23 = new Vec2(100, 100);
+       Vec2 vec24 = new Vec2(100, 200);
 
        Polygon polygon = new Polygon(4, style);
-       polygon.setPoint(0, point1);
-       polygon.setPoint(1, point2);
-       polygon.setPoint(2, point3);
-        polygon.setPoint(3, point4);
+       polygon.setPoint(0, vec21);
+       polygon.setPoint(1, vec22);
+       polygon.setPoint(2, vec23);
+        polygon.setPoint(3, vec24);
 
         SvgScene svgScene = new SvgScene();
         svgScene.addPolygon(polygon);
--- src/Polygon.java
+++ src/Polygon.java
@@ -1,33 +1,41 @@
 import java.util.Locale;
 
 public class Polygon {
-    private Point[] arr;
+    private Vec2[] arr;
     Style style;
 
     public Polygon(int count, Style style) {
-        arr = new Point[count];
+        arr = new Vec2[count];
         this.style = style;
     }
 
-    public void setPoint(int index, Point point) {
-        arr[index] = point;
+    public void setPoint(int index, Vec2 vec2) {
+        arr[index] = vec2;
     }
 
-    public void setPoints(Point[] points){
-        arr = points;
+    public void setPoints(Vec2[] vec2s){
+        arr = vec2s;
     }
 
     public String toSvg() {
         String pointsString = "";
-        for(Point point : arr)
-            pointsString += point.x + "," + point.y + " ";
+        for(Vec2 vec2 : arr)
+            pointsString += vec2.x + "," + vec2.y + " ";
 
         return String.format(Locale.ENGLISH,"<polygon points=\"%s\"%s />", pointsString, style.toSvg());
     }
 
-//    static Polygon square(){
-//
-//    }
+    static Polygon sqare(Segment line, Style style) {
+        double x = (line.getP1().x + line.getP2().x) / 2;
+        double y = (line.getP1().y + line.getP2().y) / 2;
+        Vec2 center = new Vec2(x, y);
+        Segment[] secondLine = Segment.perpendicular(line, center, line.getDistance() / 2);
+
+        Polygon polygon = new Polygon(4, style);
+        polygon.setPoint(0, line.getP1());
+        polygon.setPoint(1, secondLine[0].getP2());
+        polygon.setPoint(2, line.getP2());
+        polygon.setPoint(3, secondLine[1].getP2());
 
 
 }
