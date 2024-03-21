1d85fa504fc7627f8cde4cea2feef2ae26d991a8
patryk <kpatryk042@gmail.com>
2024-03-21 16:24:11 +0100

    kot na plikach

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,6 @@
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.IOException;
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
-import java.util.ArrayList;
+import java.util.List;
+
 
 public class Main {
     public static void main(String[] args) {
@@ -13,29 +9,38 @@ public class Main {
 //        for (String str : splited) {
 //            System.out.println(str);
 //        }
-        ArrayList<String> list = new ArrayList<>();
-        list.add("aha");
-        list.add("aha2");
-        for (String str : list) {
-            System.out.println(str);
-        }
 
+//        ArrayList<String> list = new ArrayList<>();
+//        list.add("aha");
+//        list.add("aha2");
+//        for (String str : list) {
+//            System.out.println(str);
+//        }
 
 //        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
 //        String dateString = "09.11.2001";
 //        LocalDate date = LocalDate.parse(dateString, formatter);
 
+//        try {
+//            FileReader fileReader = new FileReader("family.csv");
+//            BufferedReader reader = new BufferedReader(fileReader);
+//            String line = reader.readLine();
+//            while (line != null){
+//                line = reader.readLine();
+//                System.out.println(line);
+//            }
+//        } catch (IOException e) {
+//            throw new RuntimeException(e);
+
+        List<Person> list;
         try {
-            FileReader fileReader = new FileReader("family.csv");
-            BufferedReader reader = new BufferedReader(fileReader);
-            String line = reader.readLine();
-            while (line != null){
-                line = reader.readLine();
-                System.out.println(line);
-            }
+            list = Person.fromCsv("family.csv");
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+        for (Person person : list){
+            System.out.println(person.name);
+    }
 
 
     }
--- src/Person.java
+++ src/Person.java
@@ -1,22 +1,41 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
 
 public class Person {
     public String name;
     public LocalDate birthDate;
     public LocalDate deathDate;
 
-    public static Person fromCsvLine(String line){
+    public static Person fromCsvLine(String line) {
         String[] splited = line.split(",");
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+        LocalDate bDate = LocalDate.parse(splited[1], formatter);
         Person person = new Person();
         person.name = splited[0];
-        person.birthDate = LocalDate.parse(splited[1],formatter);
-        if (splited[2] != null) {
-            person.deathDate = LocalDate.parse(splited[2], formatter);
+        person.birthDate = bDate;
+        if (!splited[2].isEmpty()) {
+            LocalDate dDate = LocalDate.parse(splited[2], formatter);
+            person.deathDate = dDate;
         }
         return person;
     }
 
-
+    public static List<Person> fromCsv(String path) throws IOException {
+        ArrayList<Person> list = new ArrayList<>();
+        FileReader fileReader = new FileReader(path);
+        BufferedReader reader = new BufferedReader(fileReader);
+        String line = reader.readLine();
+        line = reader.readLine();
+        while (line != null) {
+            list.add(fromCsvLine(line));
+            line = reader.readLine();
+        }
+        return list;
+    }
 }
